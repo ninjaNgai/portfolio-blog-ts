1ce96c2b60b3e1e7c12caecf1c747cd1e676f648
Courtney Ngai <ctngai1010@gmail.com>
2023-02-24 20:01:44 -0700

    Start with why post

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,11 +3,6 @@ import { Box, IconButton } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import {
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerOverlay,
-  DrawerCloseButton,
   useDisclosure,
   useColorMode,
   useColorModeValue,
@@ -22,7 +17,7 @@ import { BiMenuAltRight } from 'react-icons/bi';
 import { BsMoon, BsSun } from 'react-icons/bs';
 
 const Navbar = () => {
-  const { isOpen, onToggle, onOpen, onClose } = useDisclosure();
+  const { isOpen, onToggle } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
   const router = useRouter();
 
@@ -160,67 +155,6 @@ const Navbar = () => {
       <Collapse in={isOpen} animateOpacity>
         {loadMenu(true)}
       </Collapse>
-    </Box>
-  );
-
-  const OldNavigationBar = (
-    <Box>
-      <Flex
-        bg={useColorModeValue('white', 'gray.800')}
-        color={useColorModeValue('gray.600', 'white')}
-        minH={'60px'}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.900')}
-        align={'center'}>
-        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Box display={['none', 'none', 'flex', 'flex']} alignItems="center" fontSize="1.2rem">
-            <Link href={'/'}>
-              <Heading
-                as="h4"
-                size="md"
-                textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-                fontFamily={'heading'}
-                color={useColorModeValue('gray.800', 'white')}
-                marginLeft="1rem"
-                _hover={{
-                  textDecoration: 'none',
-                  color: 'green.400'
-                }}>
-                Courtney Ngai
-              </Heading>
-            </Link>
-          </Box>
-
-          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-            <Box display={['none', 'none', 'flex', 'flex']} alignItems="center" fontSize="1rem">
-              {loadMenu()}
-            </Box>
-          </Flex>
-        </Flex>
-        <Flex justifyContent="end" alignItems="center">
-          <Box m="1rem">
-            <IconButton
-              icon={colorMode === 'light' ? <BsMoon /> : <BsSun />}
-              aria-label="change mode"
-              borderRadius="md"
-              onClick={toggleColorMode}
-            />
-          </Box>
-        </Flex>
-        <Box m="1rem" display={['flex', 'flex', 'none', 'none']}>
-          <BiMenuAltRight fontSize="2.5em" cursor="pointer" onClick={onOpen} />
-          <Drawer size="full" onClose={onClose} isOpen={isOpen}>
-            <DrawerOverlay />
-            <DrawerContent>
-              <DrawerCloseButton />
-              <DrawerBody>{loadMenu(true)}</DrawerBody>
-            </DrawerContent>
-          </Drawer>
-        </Box>
-      </Flex>
     </Box>
   );
 
