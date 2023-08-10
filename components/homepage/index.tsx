e28fe42549b8b48b1118de9e454376c9a8880a4d
Courtney <ngaicourtney0@gmail.com>
2023-08-09 22:00:31 -0700

    4 Years at PP blog post

--- components/homepage/index.tsx
+++ components/homepage/index.tsx
@@ -5,7 +5,6 @@ import RecentBlog from '@/components/homepage/recent-blog-card';
 import RecentProject from '@/components/homepage/recent-project-card';
 import Footer from '@/components/footer';
 import { getAllGithubStars } from '@/lib/get-all-github-stars';
-import { AiOutlineGithub } from 'react-icons/ai';
 import projects from '@/data/projects';
 import router from 'next/router';
 
@@ -20,6 +19,7 @@ const Profile = ({ posts }) => {
   }, []);
 
   const [stars, setStars] = useState(0);
+  const danceYears = new Date().getFullYear() - 2001;
 
   const showProjectsPage = () => {
     router.push('/projects');
@@ -94,10 +94,10 @@ const Profile = ({ posts }) => {
           <Text color={'black.500'}>
             Throughout my life, I’ve been the tech-enthusiast of my family with a curiosity of how
             web applications work and how each component of a web app creates a harmonious system.
-            I’ve also been a dancer for 21+ years which cultivates a practice of discipline, grit,
-            and artistry/creativity. Integrating my knowledge of Software Engineering & Dance, I can
-            creatively produce real-world solutions and leverage technology to provide value to
-            users.
+            I’ve also been a dancer for {danceYears}+ years which cultivates a practice of
+            discipline, grit, and artistry/creativity. Integrating my knowledge of Software
+            Engineering & Dance, I can creatively produce real-world solutions and leverage
+            technology to provide value to users.
             <br />
             <br />
             Currently, I am a Software Engineer, Scrum Master, and Tech Lead in the PayPal Credit
